35ff21b2082f7e59b753262ee795c8f64f978319
Grayson Richard <graydragon11@gmail.com>
2024-10-11 14:04:48 -0600

    hi aloe

--- src/App.tsx
+++ src/App.tsx
@@ -19,20 +19,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <h1>HI ALOE</h1>
     </main>
   );
 }
